3e610148dbfc3fe6ea602c0975cf6f12dd6dae8b
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-23 16:56:13 +0500

    setting up flutter firebase sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:chat_app/screens/auth_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
