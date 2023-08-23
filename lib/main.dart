6f81270dd368b455d320c1da1bc2e6d2416339a6
Bilawal Mehfooz <bilawalmehfoozmalik@gmail.com>
2023-08-23 18:27:20 +0500

    setting up login screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:chat_app/screens/auth_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
